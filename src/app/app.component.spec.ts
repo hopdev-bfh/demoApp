e9305f49c24bec6bcc1a350cb709e2f6bb8eb782
hopdev <nomail@nomail.com>
2021-07-06 13:29:35 +0200

    Task 4 - testing the magicpipe

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'demoApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demoApp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('demoApp app is running!');
+    expect(app.title).toEqual('Demo App');
   });
 });
--- src/app/pipes/magicpipe.pipe.spec.ts
+++ src/app/pipes/magicpipe.pipe.spec.ts
@@ -1,8 +1,24 @@
 import { MagicpipePipe } from './magicpipe.pipe';
 
 describe('MagicpipePipe', () => {
+
+  const pipe = new MagicpipePipe();
+
   it('create an instance', () => {
     const pipe = new MagicpipePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('test if magic number is an interger', () => {
+    expect(pipe.transform('test')).toBe('The inserted text is not an integer');
+  });
+
+  it( 'test that 109 is validated', () => {
+    expect(pipe.transform('109')).toBe('Validated');
+  });
+
+  it ('test that 11 gives missing 9 message', () => {
+    expect(pipe.transform('11')).toBe('The given string contains at least a 9');
+  })
+
 });
